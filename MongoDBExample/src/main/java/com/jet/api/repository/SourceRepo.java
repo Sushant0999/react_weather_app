f21d07e5365dc228afba13ef453b902146489af3
Sushant Raj <sushantraj215@gmail.com>
2023-07-21 15:51:34 +0530

    removed mongodb

--- MongoDBExample/src/main/java/com/jet/api/repository/SourceRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jet.api.repository;
-
-import com.jet.api.dto.Source;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface SourceRepo extends MongoRepository<Source, UUID> {
-}
--- MongoDBExample/src/main/resources/db/migration/V1__CREATE_JOB_TABLE.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE jobs (
-  jobId UUID,
-  jobName VARCHAR(255),
-  jobType VARCHAR(255),
-  active BOOLEAN,
-  createdOn DATE,
-  sourceStorageProfile VARCHAR(255),
-  destinationStorageProfile VARCHAR(255)
-);
