37179529a467028362673075036b897c80bd3e57
Sushant Raj <sushantraj215@gmail.com>
2023-07-26 17:57:25 +0530

    updated main

--- frontend/src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Two() {
-  return (
-    <div>
-      <h1 align="center">About</h1>
-    </div>
-  )
-}
--- frontend/src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Three() {
-  return (
-    <div>
-    <h1 align="center">Contact</h1>
-    </div>
-  )
-}
