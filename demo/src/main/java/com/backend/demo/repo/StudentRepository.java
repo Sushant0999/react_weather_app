b218ad8481cb24be2c24155421fa20f5e1ce7787
Sushant Raj <66193982+Sushant0999@users.noreply.github.com>
2023-07-26 18:46:25 +0530

    Delete demo directory

--- demo/src/main/java/com/backend/demo/repo/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.demo.repo;
-
-import com.backend.demo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
