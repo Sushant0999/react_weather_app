0b841c48c7a04a582254d0650e637c852efb2e8a
Sushant Raj <66193982+Sushant0999@users.noreply.github.com>
2023-07-27 10:08:03 +0530

    Delete MongoDBExample directory

--- MongoDBExample/src/main/java/com/jet/api/validation/EmailValidator.java
+++
@@ -1,5 +0,0 @@
-package com.jet.api.validation;
-
-public interface EmailValidator {
-     boolean emailValidator(String email);
-}
--- MongoDBExample/src/main/java/com/jet/api/validation/UserNameValidator.java
+++
@@ -1,7 +0,0 @@
-package com.jet.api.validation;
-
-import com.jet.api.enums.UsernameEnum;
-
-public interface UserNameValidator {
-    boolean userNameCheck(String username);
-}
